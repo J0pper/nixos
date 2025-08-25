f1332f7eed00edc4f115b1b152de6b7629fa5edd
Jeppe Vad Andersen <Jeppe>
2025-08-25 17:48:34 +0200

    Flakenized nixos config

---
+++ flake.nix
@@ -0,0 +1,48 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    # nixpkgs.url = "github:NixOS/nixpkgs/release-24.05";
+
+    disko.url = "github:nix-community/disko";
+    disko.inputs.nixpkgs.follows = "nixpkgs";
+
+    hyprland.url = "github:hyprwm/Hyprland";
+
+    zen-browser = {
+      url = "github:youwen5/zen-browser-flake";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { nixpkgs, ... } @inputs:
+  let
+    system = "x86_64-linux";
+  in 
+  {
+    nixosConfigurations = {
+      personal = nixpkgs.lib.nixosSystem {
+        specialArgs = { 
+          inherit inputs;
+        };
+        modules = [
+          ./hosts/personal/configuration.nix
+          ./nixosModules/boot/grub2.nix
+          ./nixosModules/services/hyprland.nix
+          ./nixosModules/services/xdg.nix
+          ./nixosModules/services/sddm.nix
+        ];
+      };
+      # work = nixpkgs.lib.nixosSystem
+
+      tv-media-box = nixpkgs.lib.nixosSystem {
+        modules = [
+          ./hosts/tv-media-box/configuration.nix
+          ./nixosModules/boot/grub2.nix
+          ./disko-configs/tv-media-box.nix
+          #inputs.disko.nixosModules.disko
+        ];
+    };
+  };
+}
