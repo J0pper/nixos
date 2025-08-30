111880b24cbd9880735497313ec38ff9d9482904
Jeppe Vad Andersen <jeppe.vadandersen@gmail.com>
2025-08-30 16:15:31 +0000

    Added disko config file for lenovo y520 laptop

---
+++ disko-configs/lenovo-y520.nix
@@ -0,0 +1,50 @@
+{
+  disko.devices = {
+    disk = {
+      main = {
+        type = "disk";
+        device = "/dev/sda";
+        content = {
+          type = "gpt";
+          partitions = {
+            ESP = {
+              priority = 1;
+              size = "512M"; # This should be enough for UEFI boot
+              type = "EF00";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+              };
+            };
+            root = {
+              size = "100%";
+              content = {
+                type = "btrfs";
+                subvolumes = {
+                  "/root" = {
+                    mountpoint = "/";
+                  };
+
+                  "/home" = {
+                    mountOptions = [ "compress=zstd" ];
+                    mountpoint = "/home";
+                  };
+
+                  "/nix" = {
+                    mountOptions = [
+                      "compress=zstd"
+                      "noatime"
+		    ];
+	            mountpoint = "/nix";
+		  };
+                };
+                mountpoint = "/";
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}
