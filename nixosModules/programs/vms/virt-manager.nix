5d9d371c20cf52f7da369ea19c56c3af0b7f55d1
Jeppe Vad Andersen <Jeppe.vadandersen@gmail.com>
2025-10-25 09:51:29 +0200

    Added virt-manager to config

---
+++ nixosModules/programs/vms/virt-manager.nix
@@ -0,0 +1,7 @@
+{ pkgs, ... }:
+{
+  programs.virt-manager.enable = true;
+  users.groups.libvirtd.members = ["your_username"];
+  virtualisation.libvirtd.enable = true;
+  virtualisation.spiceUSBRedirection.enable = true;
+}
