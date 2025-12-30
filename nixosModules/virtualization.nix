f3e2cfc4e654e80d2a888e12838753e8de06a49a
Jeppe Vad Andersen <Jeppe.vadandersen@gmail.com>
2025-12-30 16:46:58 +0100

    Sketchy virtualization support. Mostly just downloaded the program.

---
+++ nixosModules/virtualization.nix
@@ -0,0 +1,15 @@
+{ pkgs, ... }:
+{
+  virtualisation.libvirtd = {
+    enable = true;
+    qemu = {
+      package = pkgs.qemu_kvm;
+      runAsRoot = true;
+      swtpm.enable = true;
+    };
+  };
+
+  programs.virt-manager.enable = true;
+
+  users.groups.libvirtd.members = [ "jeppe" ];
+}
