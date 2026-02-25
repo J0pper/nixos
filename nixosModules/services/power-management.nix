4fc8d94a41d9a0cac71ae8ffa26cf496ac3bd515
Jeppe Vad Andersen <Jeppe.vadandersen@gmail.com>
2026-02-25 13:48:06 +0100

    moved power management to its own file

---
+++ nixosModules/services/power-management.nix
@@ -0,0 +1,9 @@
+{
+  # manage power devices
+  services.upower.enable = true;
+
+  # manage power modes and CPU frequencies
+  services.auto-cpufreq.enable = true;
+
+  # services.power-profiles-daemon.enable = true;
+}
