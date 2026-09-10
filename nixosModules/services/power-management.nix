9b7b93e3d063c1545c79824d6edee41a1f932412
Jeppe Vad Andersen <Jeppe.vadandersen@gmail.com>
2026-09-10 23:04:37 +0200

    Power management configuration with Auto CPUfreq

--- nixosModules/services/power-management.nix
+++ nixosModules/services/power-management.nix
@@ -4,6 +4,16 @@
 
   # manage power modes and CPU frequencies
   services.auto-cpufreq.enable = true;
+  services.auto-cpufreq.settings = {
+    battery = {
+      governor = "powersave";
+      turbo = "never";
+    };
+    charger = {
+      governor = "performance";
+      turbo = "auto";
+    };
+  };
 
   # services.power-profiles-daemon.enable = true;
 }
